0375582399b6d26cbf69b77a1128d2efe8ea9ff8
Ba Le Xuan <g.lexuanba@gmail.com>
2017-08-16 11:17:19 +0200

    Added token authentication

--- fb_credentials.py
+++ fb_credentials.py
@@ -9,6 +9,7 @@ import getpass
 import contextlib
 import os
 import re
+import urllib2
 
 __version__ = (0,2,2)
 __version_string__ = '.'.join(str(x) for x in __version__)
@@ -20,25 +21,25 @@ def getInput(prompt):
     '''Wrapper around builtin function raw_input in order to mock it in tests'''
     return raw_input(prompt)
 
-def get_credentials(hgrc=None, hgPrefix='', interactive=True):
-    """When credentials are not provided in the constructor, get them from hgrc or prompt user
-       hgrc: Path to hgrc file
-       hgPrefix: prefix for user and password. Useful if the hgrc is used for multiple servers
+def get_credentials(fogbugzrc=None, fogbugzPrefix='', interactive=True):
+    """When credentials are not provided in the constructor, get them from fogbugzrc or prompt user
+       fogbugzrc: Path to fogbugzrc file
+       fogbugzPrefix: prefix for user and password. Useful if the fogbugzrc is used for multiple servers
                  with different credentials
-       interactive: If credentials not found in hgrc and this is set, prompt the user
+       interactive: If credentials not found in fogbugzrc and this is set, prompt the user
     """
-    #Search whether there is an hgrc file. Default: ~/hgrc
+    #Search whether there is an fogbugzrc file. Default: ~/fogbugzrc
     username = None
     password = None
-    if not hgrc:
-        hgrc = os.path.join(os.path.expanduser("~"), '.hgrc')
-    if os.path.isfile(hgrc):
-        for line in open(hgrc):
+    if not fogbugzrc:
+        fogbugzrc = os.path.join(os.path.expanduser("~"), '.fogbugzrc')
+    if os.path.isfile(fogbugzrc):
+        for line in open(fogbugzrc):
             line = line.split('#')[0]
-            if hgPrefix + 'username' in line:
+            if fogbugzPrefix + 'username' in line:
                 res = re.search(r'username\s*=\s*(\S+)', line)
                 username = res.group(1)
-            elif hgPrefix + 'password' in line:
+            elif fogbugzPrefix + 'password' in line:
                 res = re.search(r'password\s*=\s*(\S+)', line)
                 password = res.group(1)
     if interactive:
@@ -48,8 +49,40 @@ def get_credentials(hgrc=None, hgPrefix='', interactive=True):
             password = getpass.getpass('password: ')# Same as raw_input but hide what user types
     return username, password
 
-def FogBugz(fbConstructor, hostname, token=None, username=None, password=None, hgrc=None,
-            hgPrefix='', interactive=True, storeCredentials=False):
+def get_token(fogbugzrc=None, fogbugzPrefix=''):
+    """When credentials are not provided in the constructor, get them from fogbugzrc or prompt user
+       fogbugzrc: Path to fogbugzrc file
+       fogbugzPrefix: prefix for token. Useful if the fogbugzrc is used for multiple servers
+                 with different credentials
+    """
+    # Search whether there is an fogbugzrc file. Default: ~/.fogbugzrc
+    token = None
+    if not fogbugzrc:
+        fogbugzrc = os.path.join(os.path.expanduser("~"), '.fogbugzrc')
+    if os.path.isfile(fogbugzrc):
+        for line in open(fogbugzrc):
+            line = line.split('#')[0]
+            if fogbugzPrefix + 'token' in line:
+                res = re.search(r'token\s*=\s*(\S+)', line)
+                token = res.group(1)
+    return token
+
+def validate_token(hostname, token):
+    """
+    Validate the user token.
+
+    Returns True for a successful validation.
+    """
+    url = hostname + ('/' if hostname[-1] != '/' else '') + "api.asp?cmd=logon&token=" + token
+    try:
+        response = urllib2.urlopen(url)
+        return token in response.read()
+    except Exception as e:
+        print e
+    return False
+
+def FogBugz(fbConstructor, hostname, token=None, username=None, password=None, fogbugzrc=None,
+            fogbugzPrefix='', interactive=True, storeCredentials=False):
     """Calls the constructor specified by fbConstructor (hence, despite this being a function use
         CapWords naming convention)
 
@@ -57,18 +90,22 @@ def FogBugz(fbConstructor, hostname, token=None, username=None, password=None, h
                        kiln.Kiln
        hostname: passed directly to the fbInterface
        token, username, password: input credentials
-       hgrc, hgPrefix, interactive: Passed to method get_credentials
+       fogbugzrc, fogbugzPrefix, interactive: Passed to method get_credentials
        storeCredentials: If active, create attributes token, username and password. This opens the
                           door to using it for login to other system, which is convenient, but the
                           programmer can also do what he wants with the password (which is bad).
        TODO: Support passing a list of args to fbConstructor
     """
     if token and (username or password):
-        raise TypeError("if you supply 'token' you cannot supply 'username' or 'password'")
+        raise TypeError("If you supply 'token' you cannot supply 'username' or 'password'")
     if (username and not password) or (not username and password):
         raise TypeError("You must supply both 'username' and 'password'")
+    if not token:
+        token = get_token(fogbugzrc, fogbugzPrefix)
+    if token and validate_token(hostname, token):
+        return fbConstructor(hostname, token=token)
     if not username and not token:
-        username, password = get_credentials(hgrc, hgPrefix, interactive)
+        username, password = get_credentials(fogbugzrc, fogbugzPrefix, interactive)
         if not username and password: # If still no credentials available, raise
             raise TypeError("You must provide either 'username' and 'password' or 'token'")
 
@@ -88,5 +125,8 @@ def FogBugz_cm(fbConstructor, hostname, **kwargs):
     '''Context manager with logOff functionality'''
     fb = FogBugz(fbConstructor, hostname, **kwargs)
     yield fb
-
-    fb.logoff()
+    
+    if kwargs.get('logoff', True):
+        fb.logoff()
+    else:
+        print "Save this token for later: token=%s" % fb._token
